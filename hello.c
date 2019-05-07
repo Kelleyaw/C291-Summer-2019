fbaf29dc70db9d3e6ac0eeccfbd351c32543c576
Kelleyaw <45922738+Kelleyaw@users.noreply.github.com>
2019-05-07 19:43:42 -0400

    Add files via upload
    Code for assignment 0

---
+++ hello.c
@@ -0,0 +1,20 @@
+/* 
+ * Prints out My name and state of birth 
+ * Author: Alexander Kelley
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Alexander Kelley, ", "Indiana");
+	/*
+  int myNumber = 0;
+  printf("Hello: Welcome to C291!\n");
+  printf("Please enter a number and press ENTER: ");
+  scanf("%d", &myNumber);  
+  printf("Here is the number yor entered: %d\n",myNumber);
+  return(0); 
+  */
+}
+
+
