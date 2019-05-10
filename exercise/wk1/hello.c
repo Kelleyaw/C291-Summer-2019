a3cd84e9db8351f24e15eeb8212f7a6213624a37
Kelleyaw <kelleyaw@iu.edu>
2019-05-09 22:28:27 -0400

    Update hello.c

--- exercise/wk1/hello.c
+++ exercise/wk1/hello.c
@@ -23,13 +23,5 @@ int main(void) {
 		printf("\n");
 	}
 	return 0;
-}
-	/*
-	int myNumber = 0;
-  printf("Hello: Welcome to C291!\n");
-  printf("Please enter a number and press ENTER: ");
-  scanf("%d", &myNumber);
-  printf("Here is the number yor entered: %d\n",myNumber);
-	return(0);
-  */
+ }
 }
